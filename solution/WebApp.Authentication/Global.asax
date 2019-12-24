8766f3ffde6f6ceeba95f8bc5bab56f8752f0f90
begli nazarov <bndevops@hotmail.com>
2019-12-24 17:23:35 +0300

    init-webapp.authentiocation project

---
+++ solution/WebApp.Authentication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApp.Authentication.MvcApplication" Language="C#" %>
---
+++ solution/WebApp.Authentication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApp.Authentication.Startup))]
+namespace WebApp.Authentication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
